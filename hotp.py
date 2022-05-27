68425ac24a86ecdcb477f5973120603a1cd9b9e9
Matt Ellen <127148+Mellen@users.noreply.github.com>
2022-05-27 22:26:20 +0100

    get the hmac

--- hotp.py
+++ hotp.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python
+import time
+import hmac
+
+def hotp(secret, unixtime):
+    hs = hmac.new(secret, None, 'sha1')
+    fourBytes = dynamicTruncation(hs.digest())
+
+
+def dynamicTruncation(twentyBytes):
+    pass
