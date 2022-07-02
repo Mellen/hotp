5cb181ce15aa9f2c8d4231c9d6801b36a39a70cc
Matt Ellen <127148+Mellen@users.noreply.github.com>
2022-07-02 18:45:50 +0100

    added totp function

--- hotp.py
+++ hotp.py
@@ -4,6 +4,11 @@ import hmac
 import sys
 from math import ceil
 
+def totp(secret, digits):
+    p = hotp(secret, int(time.time())//30, digits)
+    pstr = f'{p:0{digits}}'
+    return pstr
+
 def hotp(secret, counter, digits):
     hs = hmac.new(secret, counter.to_bytes(8, byteorder='big'), 'sha1')
     fourByteNumber = dynamicTruncation(hs.digest())
